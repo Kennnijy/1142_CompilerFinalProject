abd4d730f426771b5e589df669367ff8422980f5
Kennijy <150828844+Kennnijy@users.noreply.github.com>
2026-06-04 15:08:50 +0800

    Fix project directory path in run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 切換到專案資料夾
-cd ~/final/1142_CompilerFinalProject/
+cd ~/1142CompilerFinalProject/final/1142_CompilerFinalProject/
 
 echo "=========================================="
 echo " 第  組編譯器期末專題"
